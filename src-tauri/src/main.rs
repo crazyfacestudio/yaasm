b66785aab2e19f0ce299d2c06e16c1a9a9cb9457
Marc Seiler <mseiler@crazyfacestudios.com>
2024-10-10 04:26:53 -0400

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,63 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// use gamedig::games::asa;
-// use rcon_client::{AuthRequest, RCONClient, RCONConfig, RCONError, RCONRequest};
-// use std::net::IpAddr;
-// use std::str::FromStr;
-
+// use std::time::Duration;
+// use rercon::{Connection, Settings};
+use rcon::{AsyncStdStream, Connection};
 
 #[tauri::command]
-fn greet(name: &str) -> String {
+async fn greet(name: &str) -> Result<String, tauri::Error> {
     println!("I was invoked from JS!");
-    // gamedig_call();
-    format!("Hello, {}!", name)
+
+    let address = "31.214.196.6:11600";
+    let conn = <Connection<AsyncStdStream>>::builder()
+        // .enable_minecraft_quirks(true)
+        .connect(address, "mommy12345678")
+        .await;
+
+    match conn {
+        Ok(mut con) => {
+            println!("connected to server: {}", address);
+            println!("Sending command: cheat listplayers");
+            match con.cmd("listplayers").await {
+                Ok(reply) => {
+                    println!("Reply from server: {}", reply);
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
+
+    // let settings = Settings {
+    //     connect_timeout: Duration::from_millis(1000),
+    //     auth_delay: Some(Duration::from_millis(1000)),
+    // };
+
+    // let connection = Connection::open("31.214.196.6:11600", "mommy12345678", settings).await;
+
+    // match connection {
+    //     Ok(mut con) => {
+    //         println!("connected to server: 31.214.196.6:11600");
+    //         println!("Sending command: cheat listplayers");
+    //         match con.exec("\ncheat listplayers\n").await {
+    //             Ok(reply) => {
+    //                 println!("Reply from server: {}", reply);
+    //             }
+    //             Err(e) => {
+    //                 println!("Error: {:?}", e);
+    //             }
+    //         }
+    //     }
+    //     Err(e) => {
+    //         println!("Error: {:?}", e);
+    //     }
+    // }
+
+    Ok(format!("Hello, {}!", name))
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -18,14 +65,14 @@ fn main() {
     let mut ctx = tauri::generate_context!();
 
     tauri::Builder::default()
-    .plugin(tauri_plugin_theme::init(ctx.config_mut()))
-        .plugin(tauri_plugin_window_state::Builder::default().build())
+        // .plugin(tauri_plugin_single_instance::init())
+        .plugin(tauri_plugin_theme::init(ctx.config_mut()))
+        // .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(ctx)
         .expect("error while running tauri application");
 }
-
 
 // fn testing() -> Result<(), RCONError> {
 //     println!("Running RCON test");
