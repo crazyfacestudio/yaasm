a1f53131593e2ad85c64ee05f0c1a16bb05ea7d1
Marc Seiler <mseiler@crazyfacestudios.com>
2024-10-10 17:59:37 -0400

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,63 +1,39 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// use std::time::Duration;
-// use rercon::{Connection, Settings};
+use gamedig::games::epic::asa;
+use std::{net::IpAddr, str::FromStr};
+
+use anyhow::anyhow;
 use rcon::{AsyncStdStream, Connection};
 
 #[tauri::command]
 async fn greet(name: &str) -> Result<String, tauri::Error> {
     println!("I was invoked from JS!");
+    gamedig_call();
+    Ok(format!("Hello, {}!", name))
+}
+
+#[tauri::command]
+async fn rcon_command(_server_id: &str, command: &str) -> Result<String, tauri::Error> {
+    println!("I was invoked from JS!");
 
     let address = "31.214.196.6:11600";
     let conn = <Connection<AsyncStdStream>>::builder()
-        // .enable_minecraft_quirks(true)
+        .enable_factorio_quirks(true)
         .connect(address, "mommy12345678")
         .await;
 
     match conn {
         Ok(mut con) => {
             println!("connected to server: {}", address);
-            println!("Sending command: cheat listplayers");
-            match con.cmd("listplayers").await {
-                Ok(reply) => {
-                    println!("Reply from server: {}", reply);
-                }
-                Err(e) => {
-                    println!("Error: {:?}", e);
-                }
+            println!("Sending command: {}", command);
+            match con.cmd(command).await {
+                Ok(reply) => Ok(reply),
+                Err(e) => Err(anyhow!("Command Error: {:?}", e).into()),
             }
         }
-        Err(e) => {
-            println!("Error: {:?}", e);
-        }
+        Err(e) => Err(anyhow!("Connection Error: {:?}", e).into()),
     }
-
-    // let settings = Settings {
-    //     connect_timeout: Duration::from_millis(1000),
-    //     auth_delay: Some(Duration::from_millis(1000)),
-    // };
-
-    // let connection = Connection::open("31.214.196.6:11600", "mommy12345678", settings).await;
-
-    // match connection {
-    //     Ok(mut con) => {
-    //         println!("connected to server: 31.214.196.6:11600");
-    //         println!("Sending command: cheat listplayers");
-    //         match con.exec("\ncheat listplayers\n").await {
-    //             Ok(reply) => {
-    //                 println!("Reply from server: {}", reply);
-    //             }
-    //             Err(e) => {
-    //                 println!("Error: {:?}", e);
-    //             }
-    //         }
-    //     }
-    //     Err(e) => {
-    //         println!("Error: {:?}", e);
-    //     }
-    // }
-
-    Ok(format!("Hello, {}!", name))
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -69,51 +45,29 @@ fn main() {
         .plugin(tauri_plugin_theme::init(ctx.config_mut()))
         // .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, rcon_command])
         .run(ctx)
         .expect("error while running tauri application");
 }
 
-// fn testing() -> Result<(), RCONError> {
-//     println!("Running RCON test");
-//     // Create new RCON client
-//     let mut client = RCONClient::new(RCONConfig {
-//         url: "207.244.127.182:11480".to_string(),
-//         // Optional
-//         read_timeout: Some(13),
-//         write_timeout: Some(37),
-//     })?;
+fn gamedig_call() {
+    let server_ip = "31.214.196.6";
+    let server_port = Some(5600);
 
-//     // Auth request to RCON server (SERVERDATA_AUTH)
-//     let auth_result = client.auth(AuthRequest::new("mommy12345678".to_string()))?;
-//     assert!(auth_result.is_success());
+    let ip: IpAddr = IpAddr::from_str(server_ip).expect("Invalid IP address");
 
-//     // Execute command request to RCON server (SERVERDATA_EXECCOMMAND)
-//     let list_players = client.execute(RCONRequest::new("ListPlayers".to_string()))?;
-//     println!("Result: {}", list_players.body);
-//     // assert_eq!(version.body, "Seed: [3257840388504953787]");
-
-//     Ok(())
-// }
-
-// fn gamedig_call() {
-//     let server_ip = "207.244.127.182";
-//     let server_port = Some(5480);
-
-//     let ip: IpAddr = IpAddr::from_str(server_ip).expect("Invalid IP address");
-
-//     match asa::query(&ip, server_port) {
-//         Ok(response) => {
-//             println!("Server: {:?}", response);
-//             println!("Server name: {}", response.name);
-//             println!(
-//                 "Players: {}/{}",
-//                 response.players_online, response.players_maxmimum
-//             );
-//             for player in response.players {
-//                 println!("Player: {}", player.name);
-//             }
-//         }
-//         Err(e) => eprintln!("Failed to query server: {}", e),
-//     }
-// }
+    match asa::query(&ip, server_port) {
+        Ok(response) => {
+            println!("Server: {:#?}", response);
+            println!("Server name: {}", response.name);
+            println!(
+                "Players: {}/{}",
+                response.players_online, response.players_maxmimum
+            );
+            for player in response.players {
+                println!("Player: {}", player.name);
+            }
+        }
+        Err(e) => eprintln!("Failed to query server: {}", e),
+    }
+}
