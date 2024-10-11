186ed35668315112d77e714df5bdba889e008869
Marc Seiler <mseiler@crazyfacestudios.com>
2024-10-10 22:54:19 -0400

    .

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	// import { setContext, untrack } from 'svelte';
+	// import { initFlowbite } from 'flowbite';
+	import '../app.postcss';
+
+	const { children } = $props();
+</script>
+
+{@render children()}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,7 +47,7 @@
 		<ul class="space-y-2 font-medium">
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -69,7 +69,7 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -92,7 +92,7 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -115,7 +115,7 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -134,7 +134,7 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -153,7 +153,7 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
@@ -176,7 +176,7 @@
 			</li>
 			<li>
 				<a
-					href="#"
+					href="/"
 					class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
 				>
 					<svg
